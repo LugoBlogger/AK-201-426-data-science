8814b2569e217f60dea6e7b3fde909f35cf6b93f
LugoBlogger <lugoblogger@gmail.com>
2023-03-03 08:05:29 +0800

    first version

---
+++ scratch/linear_algebra.py
@@ -0,0 +1,18 @@
+from typing import List
+
+Vector = List[float]
+
+
+class LinearAlgebra(object):
+  def __init__(self):
+    return None
+
+  def dot(v: Vector, w: Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * w_n"""
+    assert len(v) == len(w), "vectors must be the same length"
+
+    return sum(v_i + w_i for v_i, w_i in zip(v, w))
+
+  def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
