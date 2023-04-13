0653c1e246aa2cfc64d8320c3e2be7b994129af1
LugoBlogger <lugoblogger@gmail.com>
2023-04-13 15:26:02 +0800

    chapter 12, but previous chapters unfinished

--- scratch/linear_algebra.py
+++ scratch/linear_algebra.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import List, Callable
 
 Vector = List[float]
@@ -7,6 +8,11 @@ Matrix = List[List[float]]
 class LinearAlgebra(object):
   def __init__(self):
     return None
+
+  def subtract(v: Vector, w: Vector) -> Vector:
+    """Subtracts corresponding elements"""
+    assert len(v) == len(w), "vectors must be the same length"
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
 
   def dot(v: Vector, w: Vector) -> float:
     """Computes v_1 * w_1 + ... + v_n * w_n"""
@@ -27,4 +33,13 @@ class LinearAlgebra(object):
     """
     return [[entry_fn(i,j)                  # given i, create a list
               for j in range(num_cols)]     # [entry_fn(i, 0), ...]
-                for i in range(num_rows)]   # create one list for each i
+                for i in range(num_rows)]   # create one list for each i  
+
+  def squared_distance(v: Vector, w: Vector) -> float:
+    """Computes (v_1 - w_1)**2 + ... + (v_n - w_n)**2"""
+    return LinearAlgebra.sum_of_squares(subtract(v, w))
+
+  def distance(v: Vector, w: Vector) -> float:    
+    """Computes the distance between v and w"""
+    return np.sqrt(LinearAlgebra.squared_distance(v, w))
+
