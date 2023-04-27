2a63f89ff6a576d5fd0906ddd2133e939c48bc64
LugoBlogger <lugoblogger@gmail.com>
2023-04-27 14:45:16 +0800

    finish clustering chapter

--- scratch/linear_algebra.py
+++ scratch/linear_algebra.py
@@ -14,6 +14,28 @@ class LinearAlgebra(object):
     assert len(v) == len(w), "vectors must be the same length"
     return [v_i - w_i for v_i, w_i in zip(v, w)]
 
+  def vector_sum(vectors: List[Vector]) -> Vector:
+    """Sum all corresponding elements"""
+    # Check that vectors is not empty
+    assert vectors, "no vectors provided!"
+
+    # Check the vectors are all the same size
+    num_elements = len(vectors[0])
+    assert all(len(v) == num_elements for v in vectors), "different sizes!"
+
+    # the i-th element of the result is the sum of every vector[i]
+    return [sum(vector[i] for vector in vectors)
+            for i in range(num_elements)]
+
+  def scalar_multiply(c: float, v: Vector) -> Vector:
+    """Multiplies every element by c""" 
+    return [c * v_i for v_i in v]
+
+  def vector_mean(vectors: List[Vector]) -> Vector:
+    """Computes the element-wise average""" 
+    n = len(vectors)
+    return LinearAlgebra.scalar_multiply(1/n, LinearAlgebra.vector_sum(vectors))
+
   def dot(v: Vector, w: Vector) -> float:
     """Computes v_1 * w_1 + ... + v_n * w_n"""
     assert len(v) == len(w), "vectors must be the same length"
