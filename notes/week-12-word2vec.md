6894f5ed58f06321d9d8cb0d00d6d1e248091d1f
LugoBlogger <lugoblogger@gmail.com>
2024-04-16 16:47:34 +0800

    partially week-12

---
+++ notes/week-12-word2vec.md
@@ -0,0 +1,24 @@
+# Notes - Week 12 - natural Language Processing - `word2vec`
+
+Lecturer: **Henokh Lugo Hariyanto**
+
+> The tips for learning how to code with Python for this lecture is you should
+> rewrite all the codes. After that you should play around by modifying all
+> the values, input parameters, and other settings. By looking at the behavior
+> of after you re run the program for several times, you should understand the 
+> mechanism and functionality of each code in a part of whole program.
+
+This note is short version and summary of (Gruz, 2019). Most of the codes and
+text are excerpt from that book.
+
+## Word Clouds
+
+## $n$-Gram Language Models
+
+## Grammers
+
+## An Aside: Gibbs Sampling
+
+## Topic Modeling
+
+## Word Vectors
--- scratch/deep_learning.py
+++ scratch/deep_learning.py
@@ -6,6 +6,38 @@ from scratch.linear_algebra import LinearAlgebra as la
 
 Tensor = list
 
+class Layer(object):
+  """Our neural networks will be composed of Layers, each of which
+     knows how to do some computation on its inputs in the `forward`
+     direction and propagate gradients in the `backward` direction."""
+
+  def forward(self, input):
+    """Note the lack of types. We're not going to be prescriptive
+       about what kinds of inputs layers can take and what kinds
+       of outputs they can return.""" 
+    
+    raise NotImplementedError 
+
+
+  def backward(self, gradient):
+    """Similarly, we're not going to be prescriptive about what the
+       gradient looks like. It's up to you the user to make sure
+       that you're doing things sensibly.""" 
+    
+    raise NotImplementedError
+
+
+  def params(self) -> Iterable[Tensor]:
+    """Returns the parameters of this layer. The default implementation
+       returns nothing, so that if you have a layer with no parameters
+       you don't have to implement this.""" 
+
+    return ()
+
+
+  def grads(self) -> Iterable[Tensor]:
+    """Returns the gradients, in the same order as params().""" 
+    return ()
 
 class DeepLearning(object):
   def shape(tensor: Tensor) -> List[int]:
@@ -123,39 +155,6 @@ class DeepLearning(object):
     for param, weight in zip(model.params(), weights):
       param[:] = weight
 
-
-class Layer(object):
-  """Our neural networks will be composed of Layers, each of which
-     knows how to do some computation on its inputs in the `forward`
-     direction and propagate gradients in the `backward` direction."""
-
-  def forward(self, input):
-    """Note the lack of types. We're not going to be prescriptive
-       about what kinds of inputs layers can take and what kinds
-       of outputs they can return.""" 
-    
-    raise NotImplementedError 
-
-
-  def backward(self, gradient):
-    """Similarly, we're not going to be prescriptive about what the
-       gradient looks like. It's up to you the user to make sure
-       that you're doing things sensibly.""" 
-    
-    raise NotImplementedError
-
-
-  def params(self) -> Iterable[Tensor]:
-    """Returns the parameters of this layer. The default implementation
-       returns nothing, so that if you have a layer with no parameters
-       you don't have to implement this.""" 
-
-    return ()
-
-
-  def grads(self) -> Iterable[Tensor]:
-    """Returns the gradients, in the same order as params().""" 
-    return ()
 
 
 class Linear(Layer):
